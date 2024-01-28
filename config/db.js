38553996cac56a77a5a82f269fc39e0e2301b231
Adarsh kumar <122091788+ADARSH9358@users.noreply.github.com>
2024-01-28 16:49:35 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,13 +1,19 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
 import colors from "colors";
+dotenv.config();
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URL);
-    console.log(
-      `Conneted To Mongodb Databse ${conn.connection.host}`.bgMagenta.white
-    );
+     await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('Connected to MongoDB');
+    
   } catch (error) {
-    console.log(`Errro in Mongodb ${error}`.bgRed.white);
+  console.error('Error connecting to MongoDB:', error.message);
+    // Handle the error appropriately, e.g., throw an error or exit the application.
+    process.exit(1);
   }
 };
 
