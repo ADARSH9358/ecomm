4601559f3dd49b6b9bcb0c866c70a78a936137c1
Adarsh kumar <122091788+ADARSH9358@users.noreply.github.com>
2024-01-28 16:51:45 +0530

    Update server.js

--- server.js
+++ server.js
@@ -81,6 +81,8 @@ const __dirname = path.dirname(__filename);
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
+const mode = process.env.DEV_MODE || 'development';
+const PORT = process.env.PORT || 8080;
 
 // Dynamic static file path based on environment
 const staticPath = process.env.NODE_ENV === 'production' ? 'client/build' : './client/public';
@@ -97,9 +99,9 @@ app.use("*", function(req, res) {
 });
 
 // PORT
-const PORT = process.env.PORT || 8080;
+
 
 // Run the server
 app.listen(PORT, () => {
-  console.log(`Server running in ${process.env.NODE_ENV || 'development'} mode on port ${PORT}`.bgCyan.white);
+  console.log(`Server running in mode on port ${PORT}`.bgCyan.white);
 });
